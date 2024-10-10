ef067e426f6e81e2a76957bcd89b934613ad964d
Jairo-Carbajal <168127511+Jairo-Carbajal@users.noreply.github.com>
2024-10-09 23:57:06 -0300

    Update navbar.jsx

--- src/Componentes/navbar.jsx
+++ src/Componentes/navbar.jsx
@@ -1,22 +1,41 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../css/NavBar.css";
 
 function NavBar() {
-    
-    return (
-        <>
-        <div className="nav">
-            <div className="titulo">
-                <Link to={"/"}>
-                    <h1>Lincoln</h1>
-                </Link>
-            </div>
-            <div className="acciones">
-                <Link to={"/venta"} >Venta</Link>
-                <Link to={"/encuesta"} >Encuesta</Link>
-            </div>
+  const location = useLocation();
+
+  const getHeaderText = () => {
+    switch (location.pathname) {
+      case "/venta":
+        return "Crear Venta";
+      case "/encuesta":
+        return "Encuesta de Satisfaccion";
+      case "/":
+        return "Estado de las Galletas";
+      default:
+        return "Bienvenido";
+    }
+  };
+  return (
+    <>
+      <div className="nav">
+        <div className="titulo">
+          <h1>LINCOLN</h1>
         </div>
-        </>
-    ); 
+        <div className="acciones">
+          <Link to={"/"} className="personal">
+            <h2>Estado Galletas</h2>
+          </Link>
+          <Link to={"/venta"} className="personal">
+            <h2>Nueva Venta</h2>
+          </Link>
+          <Link to={"/encuesta"} className="personal">
+            <h2>Encuesta</h2>
+          </Link>
+        </div>
+        <div className="locacion">{getHeaderText()}</div>
+      </div>
+    </>
+  );
 }
 export default NavBar;
